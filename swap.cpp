4560026907dd96e7e137525770b1d75ba9185a02
tejswinibhosale2007-19092007 <tejswinibhosale2007@gmail.com>
2026-07-16 21:43:22 +0530

    Update swap.cpp

--- swap.cpp
+++ swap.cpp
@@ -1,10 +1,13 @@
 #include<iostream>
-using namesspace std;
+using namespace std;
 int main()
 {
 int a,b;
+cout<<"Enter first no.";
 cin>>a=5;
+cout<<"Enter second no.";
 cin>>b=10;
 cout<<swap<<"a"<<b<<"b"<<a;
+swap(a,b);
 return 0;
 }
